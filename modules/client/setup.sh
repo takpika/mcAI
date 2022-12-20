f33e3ac82a73927afc775db4f3c2fcd4ad22e791
takpika <takpika@mail.takpika.jp>
2022-12-20 10:34:35 +0800

    fix issue

--- modules/client/setup.sh
+++ modules/client/setup.sh
@@ -51,7 +51,7 @@ fi
 
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 sudo apt update
-DEBIAN_FRONTEND=noninteractive sudo apt install openjdk-17-jdk python3 python-is-python3 python3-pip python3-tk python3-dev scrot git cifs-utils xinput inetutils-ping psmisc watchdog libgl1-mesa-dev -y
+DEBIAN_FRONTEND=noninteractive sudo apt install openjdk-17-jdk python3 python-is-python3 python3-pip python3-tk python3-dev scrot git cifs-utils xinput inetutils-ping psmisc watchdog libgl1-mesa-dev curl -y
 tee -a ~/.bashrc << EOF
 export PATH="~/.local/bin:\$PATH"
 EOF
