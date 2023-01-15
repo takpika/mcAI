dad188ad40d17aac46d75980eb6111892007bb22
takpika <takpika@mail.takpika.jp>
2023-01-16 02:54:30 +0800

    fix no freeze issue

--- modules/client/main.py
+++ modules/client/main.py
@@ -389,7 +389,7 @@ def force_quit():
                 break
             sleep(1)
     except:
-        pass
+        exit(10)
 
 def pos_distance(pos1, pos2):
     return sqrt((pos1[0]-pos2[0])**2+(pos1[1]-pos2[1])**2+(pos1[2]-pos2[2])**2)
