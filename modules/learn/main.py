acda028dbdd6ec0c06f6fe038f66d11e123f2a90
takpika <takpika@mail.takpika.jp>
2023-03-22 14:55:57 +0800

    fix issue

--- modules/learn/main.py
+++ modules/learn/main.py
@@ -392,7 +392,7 @@ def learn(learnIDs: list, learnFrameCount: list[int], rewards: list):
             x = x[:-1]
             x.extend(y)
             rewardEst = np.array([sum(data["health"][dp:])/(len(data["health"])-dp) for dp in range(len(data["health"]))]).reshape(len(data["health"]), 1) / 20
-            rewardEstMax = max(rewardEstMax, max(rewardEst))
+            rewardEstMax = max(rewardEstMax, np.max(rewardEst))
             y = rewardEst
             loss = critic.model.train_on_batch(x, y)
             loss_history.append(loss)
