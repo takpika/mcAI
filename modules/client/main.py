c511c6c7182385ba0fe5fee31255cee5411ffe03
takpika <takpika@mail.takpika.jp>
2023-04-11 07:35:24 +0800

    adjust random

--- modules/client/main.py
+++ modules/client/main.py
@@ -676,7 +676,7 @@ if __name__ == "__main__":
                         ai_k, ai_m, ai_mem, ai_chat = actor.predict(model.make_input(
                             x_img, x_reg, x_mem, x_reg2, x_mem2, x_name, x_mes, 1
                         ), verbose=0)
-                        if random.random() < 0.2:
+                        if random.random() < 0.3:
                             ai_k = np.random.random(ai_k.shape)
                             ai_m[0] = np.random.random(ai_m[0].shape) * 2 - 1
                             ai_m[1] = np.random.random(ai_m[1].shape)
