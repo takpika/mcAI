b1a3453aae6bc8d44180d05d62116a78170f35d2
takpika <takpika@mail.takpika.jp>
2023-05-08 15:49:44 +0800

    adjust random apple

--- modules/server/main.py
+++ modules/server/main.py
@@ -216,7 +216,7 @@ def randomApple():
         sleep(1)
     while True:
         runCommand("execute at @a run summon minecraft:item ~ ~100 ~ {Item:{id:\"minecraft:apple\",Count:4b},PickupDelay:0s,Tags:[\"randomApple\"],NoGravity:true}")
-        runCommand("execute at @r run spreadplayers ~ ~ 5 10 false @e[tag=randomApple]")
+        runCommand("execute at @r run spreadplayers ~ ~ 0 30 false @e[tag=randomApple]")
         sleep(60)
 
 def minecraftServer():
