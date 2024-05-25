05c0de0d3f37c09da7e40281efbe2cb7e3506b4f
takpika <takpika@mail.takpika.jp>
2024-05-25 18:11:40 +0800

    fix error

--- modules/client/setup.sh
+++ modules/client/setup.sh
@@ -198,7 +198,7 @@ setupMC() {
     echo "[INFO] Installing Minecraft Forge"
     CERTPATH=`python -c "import certifi; print(certifi.where())"`
     curl -o /tmp/isrg-root-x2.pem https://letsencrypt.org/certs/isrg-root-x2.pem > /dev/null
-    cat /tmp/isrg-root-x2.pem >> sudo tee -a $CERTPATH > /dev/null
+    cat /tmp/isrg-root-x2.pem | sudo tee -a $CERTPATH > /dev/null
     /opt/mcAI/bin/python modules/client/pmc.py $MC_VERSION > /dev/null
 }
 
