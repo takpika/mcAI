9727139e9dfb666b7ba6462f0fe10409300b8205
takpika <takpika@mail.takpika.jp>
2024-05-30 15:43:00 +0800

    ソート削除

--- modules/learn/main.py
+++ modules/learn/main.py
@@ -176,7 +176,6 @@ class Learn(ModuleCore):
                             daf = self.convFrame(data["data"][i], rewardEst[i])
                             img = self.videoFrames[id][i]
                             self.learnFramesBuffer.append({"data": daf, "img": img})
-                            self.learnFramesBuffer = sorted(self.learnFramesBuffer, key=lambda x: x["data"][-1], reverse=True)
                         self.moveFrames.pop(id)
                         self.videoFrames.pop(id)
                 if len(self.learnFramesBuffer) > self.LEARN_LIMIT:
